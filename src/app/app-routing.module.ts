016ce9091e6341380b55dc6b7040d55ad5d1bd0b
MiguelAngelR0 <miguelangelrodriguezescorcia@gmail.com>
2023-05-30 13:29:08 +0200

    Vista y maqueta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+
+
+
+
+
+const routes: Routes = [
+  {
+    path: '',
+    children:[
+      {
+        path: '',
+        loadChildren: () => import('src/app/pages/home-page/home-page.module').then(m => m.HomePageModule)//Cuando pones el loadChildren, hay que poner en su modulo, la propia ruta apuntando al componente
+        
+      },
+      {
+        path: 'buscador',
+        loadChildren: () => import('./pages/buscador-page/buscador-page.module').then(m => m.BuscadorPageModule)
+      },
+      {
+        path: 'detallePoliza/:id',
+        loadChildren: () => import('./pages/detalle-poliza-page/detalle-poliza-page.module').then(m => m.DetallePolizaPageModule)
+      }
+    ]
+  },
+
+];
+
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
